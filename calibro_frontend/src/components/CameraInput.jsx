2eec71a59acbdc29287f347dcfe3698f2341b5e8
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2024-10-30 19:23:51 +0530

    Point to point back to normal

--- calibro_frontend/src/components/CameraInput.jsx
+++ calibro_frontend/src/components/CameraInput.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import './CameraInput.scss'
 import storage from '../firebase_storage';
 import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import React from 'react'
-import ImageMarker, { Marker } from 'react-image-marker';
 
 const handleUpload = (file, setProgress, setImage, setStage) => {
     if (!file) return;
@@ -56,132 +54,18 @@ const uploadImage = (setImage, setStage)=>{
     </div>
 }
 
-const putPoints = (image, setStage, setInfo)=>{
-    const [markers, setMarkers] = useState([]);
-    const [errormethod, setErrormethod] = useState("geometric")
-
-    
-    const worldCoordinates = [
-        "(0, 0, 0)", "(0, 1, 0)", "(1, 0, 0)", "(0, 0, 1)", "(1, 1, 0)", "(0, 1, 1)"
-    ]
-
-    const validateInfo = ()=>{
-        if(markers.length < 6) return true
-        return false
-    }
-
-    const submitInfo = ()=>{
-        setInfo({
-            "worldCoordinates" : worldCoordinates,
-            "markers" : markers,
-            "errorMethod" : errormethod
-        })
-        setStage(2)
-    }
-
-    const numbersArray = []
-    for(var i = 0; i < 6; i++) numbersArray.push(i)
-
-    const clearImage = ()=>{
-        setMarkers([])
-    }
-    const undo = ()=>{
-        var n = markers.length
-        if(n == 0){
-            return ;
-        }
-        setMarkers(markers.slice(0, n - 1))
-    }
-    return (
-        <div className="putPoints row">
-            <div className="col-6 testImageCol">
-                <ImageMarker
-                    src={image}
-                    markers={markers}
-                    onAddMarker={(marker) => setMarkers([...markers, marker])}
-                    extraClass = "testImage"
-                /> 
-            </div>
-            <div className="info col-6 d-flex flex-column justify-content-center align-items-center">
-                <button className="btn btn-dark" onClick={undo}>
-                    Undo last placement
-                </button>
-                <button className="btn btn-dark" onClick={clearImage}>
-                    Clear all markers
-                </button>
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">Point number</th>
-                        <th scope="col">World Coordinate (x, y, z)</th>
-                        <th scope="col">Image Coordinate (u, v)</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            numbersArray.map((i, j)=>{
-                                var x = "Not marked", y = "Not marked";
-                                if(i < markers.length){
-                                    var p = markers[i]
-                                    var y = parseInt(p["top"] * 100)/100;
-                                    var x = parseInt(p["left"] * 100)/100;
-                                }
-                                return <tr>
-                                    <th scope="row">{i + 1}</th>
-                                    <td>{worldCoordinates[i]}</td>
-                                    <td>{`(${x}, ${y})`}</td>
-                                </tr>
-                            })
-                        }
-                    </tbody>
-                </table>
-
-                <div className="d-flex justify-content-evenly">
-                        <div className="optiondiv">
-                            <input type="radio" name="geometric" checked = {errormethod == "geometric"}
-                            onClick={()=>{
-                                setErrormethod("geometric")
-                            }}/> 
-                            <label for = "geometric" className='optionlbl'>Geometric</label>
-                        </div>
-                        <div className="optiondiv">
-                            <input type="radio" name="linear" checked = {errormethod == "linear"} onClick={()=>{
-                                setErrormethod("linear")
-                            }}/> 
-                            <label for = "linear" className='optionlbl' >Linear</label>
-                        </div>
-                </div>
-
-                <button className='btn btn-warning' onClick = {submitInfo}
-                 disabled = {validateInfo()}>
-                    Compute Homography
-                </button>
-            </div> 
-        </div>
-    );
-}
-
-const showOutput = (info, image)=>{
-    const [waiting, setWaiting] = useState(true)
-    if(waiting){
-        return <div className="waitingDiv d-flex justify-content-center align items-center">
-            <div class="spinner-border" role="status"></div>
-        </div>
-    }
-    return <div className="result">
-
-    </div>
+const putPoints = (image, points, i)=>{
+    return <img src={image}></img>
 }
 
 const CameraInput = ()=>{
     const [stage, setStage] = useState(0)
-    const [image, setImage] = useState("https://upload.wikimedia.org/wikipedia/commons/6/61/Rubiks_cube_solved.jpg")
-    const [info, setInfo] = useState({})
+    const [image, setImage] = useState("")
+    const [points, setPoints] = useState([])
 
     const stageMap = {
         0 : uploadImage(setImage, setStage),
-        1 : putPoints(image, setStage, setInfo),
-        2 : showOutput(info, image)
+        1 : putPoints(image, points, i)
     };
 
     return <div className="CameraInput">
